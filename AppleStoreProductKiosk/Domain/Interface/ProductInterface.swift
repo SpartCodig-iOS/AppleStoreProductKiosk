cf50b7ef6b4957c522cd62e12966e0ebfc4a58d8
Roy <shuwj81@daum.net>
2025-09-17 16:24:15 +0900

    ✨[feat]:  domain 구현

--- AppleStoreProductKiosk/Domain/Interface/ProductInterface.swift
+++ AppleStoreProductKiosk/Domain/Interface/ProductInterface.swift
@@ -1,7 +1,13 @@
 //
-//  Untitled.swift
+//  ProductInterface.swift
 //  AppleStoreProductKiosk
 //
 //  Created by 홍석현 on 9/16/25.
 //
 
+import Foundation
+
+protocol ProductInterface {
+  func fetchProductCatalog() async throws -> ProductCatalog
+  func fetchProducts(for category: String) async throws -> [Product]
+}
