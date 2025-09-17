117018b82c6b558c6edd193c6e73d890aeae462a
Roy <shuwj81@daum.net>
2025-09-17 14:02:55 +0900

    ✨[feat]: data error 정의 수정

--- AppleStoreProductKiosk/Data/Model/Error/DataError.swift
+++ AppleStoreProductKiosk/Data/Model/Error/DataError.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// 네트워크 및 데이터 처리 과정에서 발생할 수 있는 오류를 정의한 enum입니다.
-enum DataError: Error, LocalizedError, Sendable, Equatable {
+enum DataError: Error, LocalizedError, Sendable {
   /// 데이터가 없음
   case noData
 
@@ -21,6 +21,9 @@ enum DataError: Error, LocalizedError, Sendable, Equatable {
   /// HTTP 응답 오류 (응답 객체, 에러 메시지)
   case httpResponseError(HTTPURLResponse, String)
 
+  /// 디코딩 에러
+  case decodingError(Error)
+
   /// 사용자에게 표시할 에러 메시지
   var errorDescription: String? {
     switch self {
@@ -32,6 +35,8 @@ enum DataError: Error, LocalizedError, Sendable, Equatable {
       return "처리되지 않은 상태 코드: \(code)"
     case .httpResponseError(let response, let message):
       return "HTTP \(response.statusCode): \(message)"
+    case .decodingError(let error):
+      return "디코딩 실패: \(error.localizedDescription)"
     }
   }
 }
